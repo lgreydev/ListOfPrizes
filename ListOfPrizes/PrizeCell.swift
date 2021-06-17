6af61281933fe48fad6209122a678e99552cc611
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-06-17 10:07:05 +0300

    Rename 'ViewController' to 'PrizeDetailVC'

---
+++ ListOfPrizes/PrizeCell.swift
@@ -0,0 +1,8 @@
+//
+//  PrizeCell.swift
+//  ListOfPrizes
+//
+//  Created by Sergey Lukaschuk on 17.06.2021.
+//
+
+import Foundation
---
+++ ListOfPrizes/PrizeTableView.swift
@@ -0,0 +1,8 @@
+//
+//  PrizeTableView.swift
+//  ListOfPrizes
+//
+//  Created by Sergey Lukaschuk on 17.06.2021.
+//
+
+import Foundation
