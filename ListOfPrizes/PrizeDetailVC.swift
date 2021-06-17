bd0ffb99432312a7bb955201b8ace706b7a8f412
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-06-17 10:24:51 +0300

    Connect IBOutlet and IBAction to 'PrizeDetailVC'

--- ListOfPrizes/PrizeDetailVC.swift
+++ ListOfPrizes/PrizeDetailVC.swift
@@ -9,15 +9,20 @@ import UIKit
 
 class PrizeDetailVC: UIViewController {
 
+    // MARK: - IBOutlet
+    @IBOutlet weak var titleTF: UITextField!
+    @IBOutlet weak var priceTF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     
+    // MARK: - IBAction
     @IBAction func saveAction(_ sender: UIBarButtonItem) {
         
     }
-    
 }
 
